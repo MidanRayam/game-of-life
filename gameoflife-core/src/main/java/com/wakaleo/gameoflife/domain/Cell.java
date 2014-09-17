c4e73ce3b715b68f5ce9f5a6ea4c2bc05f84aa96
MidanRayam <mayar2771@gmail.com>
2014-09-17 13:14:00 +0200

    Update Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -2,16 +2,10 @@ package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
     LIVE_CELL("*"), DEAD_CELL(".");
-    int test1 = 15;
-    Long test2 = 15;
 
     private String symbol;
 
     private Cell(final String initialSymbol) {
-        
-        if(test1.equals(test2)){
-            return "Bug";
-            }
         this.symbol = initialSymbol;
     }
 
