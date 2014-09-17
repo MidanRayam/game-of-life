f207e0e215cfd838bf17ca441ec2a3e817f1e062
MidanRayam <mayar2771@gmail.com>
2014-09-17 11:56:44 +0200

    Update Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -12,7 +12,7 @@ public enum Cell {
 
     @Override
     public String toString() {
-        if(symbol == "+" || symbol == ""){
+        if(symbol == "+" || symbol == "."){
         return symbol;
         }
         else 
