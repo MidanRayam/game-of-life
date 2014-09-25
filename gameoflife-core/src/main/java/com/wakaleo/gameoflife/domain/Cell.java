881b2d74a704149ae1c2fda88d7b18d5539c9ebc
Nadim Mayar <mayar2771@gmail.com>
2014-09-25 15:56:41 +0200

    skljdf

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -4,6 +4,7 @@ public enum Cell {
     LIVE_CELL("*"), DEAD_CELL(".");
 
     private String symbol;
+	private String iets;
 	
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
